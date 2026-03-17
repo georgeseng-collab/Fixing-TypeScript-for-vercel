ec5d760f2c4fff6ac3cc27fb16aed0f38e2d332f
georgeseng-collab <george.seng@geniebook.com>
2026-03-18 01:34:30 +0800

    ping

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensures the app looks at the root for its JS/CSS
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
